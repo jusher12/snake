f720eefcc98c112a4bdaf042e870f64828946c5d
Jacob Usher <jdusher@ncsu.edu>
2023-02-25 22:02:55 -0500

    [Implementation]
    Created snake file with some functions for creating new blocks and
    checking for fail states.

--- src/snake.rs
+++ src/snake.rs
@@ -2,10 +2,12 @@ use std::collections::LinkedList;
 use piston_window::{Context, G2d};
 use piston_window::types::Color;
 
-use draw::draw_block;
+use crate::draw::draw_block;
+
 // Snake color
 const SNAKE_COLOR: Color = [0.00, 1.00, 0.00, 1.00];
 // Directions of snake / Keyboard input.
+#[derive(Copy, Clone, PartialEq)]
 pub enum Direction {
     Up,
     Down,
@@ -27,6 +29,7 @@ impl Direction {
 }
 
 // Struct for block type.
+#[derive(Debug, Clone)]
 struct Block {
     x: i32,
     y: i32,
@@ -34,7 +37,7 @@ struct Block {
 // Public struct for snake.
 pub struct Snake {
    direction: Direction,
-   body: LinkedList<Blcok>,
+   body: LinkedList<Block>,
    tail: Option<Block>,
 }
 
@@ -45,7 +48,7 @@ impl Snake {
         body.push_back(Block { x: x + 1, y});
         body.push_back(Block {x, y});
 
-        Snake { direction: Direction::Right, body, tail: None, }
+        Snake { direction: Direction::Right, body: body, tail: None, }
     }
 
     pub fn draw(&self, con: &Context, g: &mut G2d) {    
@@ -57,5 +60,79 @@ impl Snake {
     pub fn head_position(&self) -> (i32, i32) {
         let head_block = self.body.front().unwrap();
         (head_block.x, head_block.y)
+    }
+
+    pub fn move_forward(&mut self, dir: Option<Direction>) {
+        match dir {
+            Some(d) => self.direction = d,
+            None => (),
+        }
+
+        let (last_x, last_y): (i32, i32) = self.head_position();
+
+        let new_block = match self.direction {
+            Direction::Up => Block { // 0,0 is top left
+                x: last_x,
+                y: last_y - 1,
+            },
+            Direction::Down => Block {
+                x: last_x,
+                y: last_y + 1,
+            },
+            Direction::Left => Block {
+                x: last_x - 1,
+                y: last_y
+            },
+            Direction::Right => Block {
+                x: last_x + 1,
+                y: last_y,
+            },
+        };
+
+        self.body.push_front(new_block);
+        
+        let removed_block = self.body.pop_back().unwrap();
+        self.tail = Some(removed_block);
+    }
+    pub fn head_direction(&self) -> Direction {
+        self.direction
+    }
+
+    pub fn next_head(&self, dir: Option<Direction>) -> (i32, i32) {
+        let (head_x, head_y): (i32, i32) = self.head_position();
+        // Update direction if needed
+        let moving_dir = match dir {
+            Some(d) => d,
+            None => self.direction,
+        };
+
+        // Return head coord
+        match moving_dir {
+            Direction::Up => (head_x, head_y - 1),
+            Direction::Down => (head_x, head_y + 1),
+            Direction::Left => (head_x - 1, head_y),
+            Direction::Right => (head_x + 1, head_y),
+        }
+    }
+
+    pub fn restore_tail(&mut self) {
+        let block = self.tail.clone().unwrap();
+        self.body.push_back(block);
+    }
+
+    pub fn overlap_tail(&self, x: i32, y:i32) -> bool {
+        let mut ch = 0;
+        for block in &self.body {
+            if x == block.x && y == block.y {
+                return true;
+            }
+
+            ch += 1; // We don't want fail state from head
+                     // being behind tail.
+            if ch == self.body.len() - 1 {
+                break;
+            } 
+        }
+        return false;
     }
 }
