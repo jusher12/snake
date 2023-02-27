58ef23123acda8db9c1a81d6051fe06025bc0d46
Jacob Usher <jdusher@ncsu.edu>
2023-02-27 14:53:04 -0500

    [Implementation]
    Implemented main method.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-#![allow(dead_code)]
-
 // Imported Libraries
 extern crate rand;
 extern crate piston_window;
@@ -8,6 +6,35 @@ mod draw;
 mod snake;
 mod game;
 
+use piston_window::*;
+use piston_window::types::Color;
+use crate::game::Game;
+use crate::draw::to_coord_u32;
+
+const BACK_COLOR: Color = [0.5, 0.5, 0.5, 1.0];
+
 fn main() {
-    println!("Hello, world!");
+    // Hard coded game borders. Could add feature to change this and other settings through GUI
+    let (width, height) = (20, 20);
+
+    // Create game window.
+    let mut window: PistonWindow = WindowSettings::new(
+        "Snake",
+        [to_coord_u32(width), to_coord_u32(height)],
+    ).exit_on_esc(true).build().unwrap();
+
+    let mut game = Game::new(width, height);
+
+    while let Some(event) = window.next() {
+        if let Some(Button::Keyboard(key)) = event.press_args() {
+            game.key_pressed(key);
+        }
+        window.draw_2d(&event, |c, g, _| {
+            clear(BACK_COLOR, g);
+            game.draw_board(&c, g);
+        });
+        event.update(|arg| {
+            game.update(arg.dt);
+        });
+    }
 }
